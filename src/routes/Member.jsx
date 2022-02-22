ef0557a2e50e09c16d55f42dccac368c9ac08e07
ion-dci <ion.lazarev@digitalcareerinstitute.org>
2022-02-22 12:09:21 +0100

    ...

--- src/routes/Member.jsx
+++ src/routes/Member.jsx
@@ -1,10 +1,17 @@
-import { Link, Outlet } from "react-router-dom";
-import { getMembers } from "../data";
+import { useParams } from "react-router-dom";
+import { getMember } from "../data";
 
 const Member = () => {
+  let params = useParams();
+  let member = getMember(parseInt(params.memberId, 10));
+  console.log(member);
   return (
-    <div>Member</div>
-  )
-}
+    <main style={{ padding: "1rem" }}>
+      <h2>{member.name}</h2>
+      <p>Username: {member.username}</p>
+      <p>Email{member.email}</p>
+    </main>
+  );
+};
 
-export default Member
+export default Member;
