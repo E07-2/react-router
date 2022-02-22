1472e043f7efdf06035876dc176141a814ad37bb
ion-dci <ion.lazarev@digitalcareerinstitute.org>
2022-02-22 11:50:21 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Team from "./routes/Team";
 import Contact from "./routes/Contact";
+import Member from "./routes/Member";
 import "./App.css";
 
 const App = () => {
@@ -12,7 +13,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}>
           <Route path="about" element={<About />} />
-          <Route path="team" element={<Team />} />
+          <Route path="team" element={<Team />}>
+            {/* nest member route inside of team route */}
+            <Route path=":memberId" element={<Member />} />
+          </Route>
           <Route path="contact" element={<Contact />} />
         </Route>
         {/*  Add no match route  */}
---
+++ src/routes/Member.jsx
@@ -0,0 +1,10 @@
+import { Link, Outlet } from "react-router-dom";
+import { getMembers } from "../data";
+
+const Member = () => {
+  return (
+    <div>Member</div>
+  )
+}
+
+export default Member
